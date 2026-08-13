f7fd6938460a1bace6296c19cb7dd936f3d3eceb
Daniel Berruecos <danyelberruecos@gmail.com>
2026-08-13 16:41:47 -0500

    Ahora si se pone campania y actividad

--- src/main/java/co/eia/Actividad.java
+++ src/main/java/co/eia/Actividad.java
@@ -1,13 +1,31 @@
 package co.eia;
 
 public class Actividad {
+	private int codigo;
+	private String nombre;
 	private String tipo;
 	private String fecha;
 	
 	
-	
+	// constructor
 	public Actividad(String tipo, String fecha) {
 		this.tipo = tipo;
 		this.fecha = fecha;
 	}
+	
+	// getter de codigo
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	
+	//Mostrar información básica
+	public void mostrar() {
+		System.out.println("----------------------------");
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Código: " + codigo);
+		System.out.println("Fecha: " + fecha);
+		System.out.println("----------------------------");
+	}
+	
 }
